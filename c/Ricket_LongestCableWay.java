26d6247d6fac82c843651a8bd3d18ca30e58fd24
Ricket <richard@richardcarter.org>
2011-10-21 21:01:34 -0400

    updated

--- c/Ricket_LongestCableWay.java
+++ c/Ricket_LongestCableWay.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Ricket_LongestCableWay {
@@ -29,10 +30,12 @@ public class Ricket_LongestCableWay {
 		BufferedReader in = new BufferedReader(converter);
 
 		// begin your stuff here
-		int d;
+		final int d;
 		try {
 			d = Integer.parseInt(in.readLine());
 		} catch (IOException e) {
+			System.exit(1);
+			return; // stupid compiler
 		}
 		
 		String line;
@@ -40,27 +43,96 @@ public class Ricket_LongestCableWay {
 		
 		try {
 			while((line = in.readLine()) != null) {
+				if(line.equals("0 0")) break;
 				int idx = line.indexOf(' ');
 				int len = Integer.parseInt(line.substring(0, idx));
 				int quan = Integer.parseInt(line.substring(idx+1));
 				if(quan > 0) {
+					// optimize by reducing the len so that len*quan < d
+					quan = Math.min(quan, d / len);
 					inventory.add(new CableInventory(len, quan));
 				}
 			}
 		} catch (IOException e) {
 		}
 		
+		int D = 0;
+		int min = Integer.MAX_VALUE;
+		
+		Collections.sort(inventory); // this sorts inventory descending
+
+		List<Joint> joints = new ArrayList<Joint>();
+		/*
+		while(true) {
+			CableInventory largestInventory = null;
+			
+			for(CableInventory ci : inventory) {
+				if(D + ci.length > d) {
+					continue;
+				}
+				if(ci.num > 0) {
+					largestInventory = ci;
+					break;
+				}
+			}
+			
+			if(largestInventory == null) {
+				break;
+			}
+			
+			Joint j = new Joint(largestInventory);
+			joints.add(j);
+			D += largestInventory.length;
+			largestInventory.num--;
+			
+			System.out.println(D + ", joints: "+(joints.size()-1));
+			if(D == d) {
+				min = Math.min(min, (joints.size() - 1));
+				Joint removed = joints.remove(0);
+				D -= removed.length;
+				removed.ci.max--;
+			} else if(D < d) {
+				// let it continue
+			} else if(D > d) {
+				joints.remove(j);
+				D -= largestInventory.length;
+				largestInventory.max--;
+			}
+		}
+		*/
+		if(min == Integer.MAX_VALUE) {
+			System.out.println("No solution possible");
+		} else {
+			System.out.println(min);
+		}
+		System.exit(0);
 		
 		
 	}
 	
-	private static class CableInventory {
+	private static class Joint {
+		public final CableInventory ci;
 		public final int length;
-		public final int max;
+		
+		public Joint(CableInventory ci) {
+			this.ci = ci;
+			this.length = ci.length;
+		}
+	}
+	
+	private static class CableInventory implements Comparable<CableInventory> {
+		public final int length;
+		public int max;
 		public int num;
 		public CableInventory(int length, int num) {
 			this.length = length;
 			this.max = num;
+			this.num = num;
+		}
+		
+		@Override
+		public int compareTo(CableInventory o) {
+			return o.length - this.length;
 		}
 	}
 
