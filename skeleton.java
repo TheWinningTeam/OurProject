ad2d8e67af2674bda8883ec5593ffc7cc24b5cb2
Ricket <richard@richardcarter.org>
2011-10-21 20:04:13 -0400

    Added imports to skeleton file

--- skeleton.java
+++ skeleton.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class RENAMETHIS {
 	public static void main(String[] args) {
 		try {
